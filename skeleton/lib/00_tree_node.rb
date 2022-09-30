6b00d3c1515eb47e029e1051f0f69a6e54cbf4d0
Ethan Graham <ethangraham43@gmail.com>
2022-09-30 12:00:10 -0400

    polytreenode started

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        # self.parent = node
+        if node != nil
+            # self.parent = node
+            node.children << self 
+        else
+            node.parent = nil
+        end
+        # self
+    end
+
+
+
+
+    # def parent
+    #     @parent
+    # end
+
+    # def children
+    #     @children
+    # end
+
 
 end
